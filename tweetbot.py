5a5f408653658c135a22f77983974ef9060c227d
rvfer <95980994+rvfer@users.noreply.github.com>
2022-02-23 19:38:39 -0300

    Alterando tweetbot.py

--- tweetbot.py
+++ tweetbot.py
@@ -8,5 +8,8 @@ c_secret = environ["c_secret"]
 a_token = environ["a_token"]
 a_token_secret = environ["a_token_secret"]
 
-client = tweepy.Client(c_key, c_secret,	a_token, a_token_secret)
+client = tweepy.Client(
+    consumer_key=c_key, consumer_secret=c_secret,
+    access_token=a_token, access_token_secret=a_token_secret
+)
 response = client.create_tweet(text=choice(haicais))
